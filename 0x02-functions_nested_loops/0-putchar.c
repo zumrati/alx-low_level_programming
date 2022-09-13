f53234f8af18bea3402027c29ec77668508bf3a9
zumrati <zumratiy@gmail.com>
2022-09-13 14:48:29 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,41 +1,39 @@
 #include "main.h"
 
 /**
- *
- * *main - print putchar
- *
- * *
- *
- * *Return: Always 0 (success)
- *
- * */
 
+*main - print putchar
 
+*
+
+*Return: Always 0 (success)
+
+*/
 
 int main(void)
 
 {
 
-	_putchar('_');
+_putchar('_');
 
-	_putchar('p');
+_putchar('p');
 
-	_putchar('u')
+_putchar('u')
 
-	_putchar('t');
+_putchar('t');
 
-	_putchar('c');
+_putchar('c');
 
-	_putchar('h');
+_putchar('h');
 
-	_putchar('a');
+_putchar('a');
 
-	_putchar('r');
+_putchar('r');
 
-	_putchar('\n');
+_putchar('\n');
 
-	   
+   
 
-	return (0);
+return (0);
 
 }
