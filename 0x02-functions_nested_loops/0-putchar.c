9041a32aae8b3b0d6316274e019a7302166ec214
zumrati <zumratiy@gmail.com>
2022-09-14 05:05:52 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,23 @@
+In main.h
+
+int _putchar(char);
+
+
+In 0-putchar
+
 #include "main.h"
 
 /**
+ *main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
+ * Return: Always 0 (success)
+ */
+int _putchar(char)
 {
+	putchar(');
+	_pitchar('p');
 
-		char *sh = "_putchar";
-
-
-
-			while (*sh)
-
-					{
-
-								_putchar(*sh);
-
-										sh++;
-
-											}
-
-				_putchar('\n');
-
-
-
-					return (0);
-
+	onward
+	return(0);
 }
+
