fbc04928f526b7d9cf250788e417e765ff184315
zumrati <zumratiy@gmail.com>
2022-09-16 00:00:23 -0700

    1. Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - Causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i:
+		Printf("Infinite loop incoming: (\
+n")
+		i = 0;
+	/*
+	 * while(i < 10)
+	 * {
+	 * putchar (i);
+	 * }
+	 */
+}
