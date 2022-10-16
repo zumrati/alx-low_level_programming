f6f58fbb71cd9b63bb6c95aabcf2662384dad7b4
zumrati <zumratiy@gmail.com>
2022-10-16 04:02:21 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#indef FUNCTION_POINTERS_H
+#def FUNCTION_POINTERS_H
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int sizes, int (cmp)(int));
+
+#endif
